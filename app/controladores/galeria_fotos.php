3cbf3040cb391ebc1636c09cacac05bc7bda86bc
Jesus Maria de Quevedo Tome <jequeto@gmail.com>
2013-02-22 14:08:27 +0100

    22/02/2013 14:00

--- app/controladores/galeria_fotos.php
+++ app/controladores/galeria_fotos.php
@@ -103,94 +103,22 @@ class galeria_fotos extends \core\Controlador {
 
 	
 	
-	public function form_modificar(array $datos=array()) {
-		if ( ! count($datos)) { // Si no es un reenvío desde una validación fallida
-			$validaciones=array(
-				"id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/articulos/id"
-			);
-			if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
-				$datos['mensaje'] = 'Datos erróneos para identificar el artículo a modificar';
-				$this->cargar_controlador('mensajes', '', $datos);
-				return;
-			}
-			else {
-				$clausulas['where'] = " id = {$datos['values']['id']} ";
-				if ( ! $filas = \datos\Datos_SQL::select('articulos', $clausulas)) {
-					$datos['mensaje'] = 'Error al recuperar la fila de la base de datos';
-					$this->cargar_controlador('mensajes', '', $datos);
-					return;
-				}
-				else {
-					$datos['values'] = $filas[0];
-					$datos['values']['precio'] = \core\Conversiones::decimal_punto_a_coma_y_miles($datos['values']['precio']);
-					$datos['values']['unidades_stock'] = \core\Conversiones::decimal_punto_a_coma_y_miles($datos['values']['unidades_stock']);
-				}
-			}
-		}
-		print_r($datos);
-		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
-		\core\Respuesta::enviar($datos);
-	}
-
-	public function validar_form_modificar(array $datos=array())
-	{	
-		$validaciones=array(
-			 "id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/articulos/id"
-			, "nombre" =>"errores_requerido && errores_texto && errores_unicidad_modificar:id,nombre/articulos/nombre,id"
-			, "precio" => "errores_precio"
-			, "unidades_stock" => "errores_precio"
-		);
-		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
-			print_r($datos);
-            $datos["errores"]["errores_validacion"] = "Corrige los errores.";
-		}
-		else {
-			$datos['values']['precio'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['precio']);
-			$datos['values']['unidades_stock'] = \core\Conversiones::decimal_coma_a_punto($datos['values']['unidades_stock']);
-			if ( ! $validacion = \datos\Datos_SQL::update($datos["values"], 'articulos')) // Devuelve true o false
-				$datos["errores"]["errores_validacion"]="No se han podido grabar los datos en la bd.";
-		}
-		if ( ! $validacion) //Devolvemos el formulario para que lo intente corregir de nuevo
-			$this->form_modificar($datos);
-		else
-		{
-			$datos = array("alerta" => "Se han modificado correctamente.");
-			// Definir el controlador que responderá después de la inserción
-			$this->index($datos);		
-		}
-	}
-
+	
 	
 	
 	public function form_borrar(array $datos=array()) {
 		
-		$validaciones=array(
-			"id" => "errores_requerido && errores_numero_entero_positivo && errores_referencia:id/articulos/id"
-		);
-		if ( ! $validacion = ! \core\Validaciones::errores_validacion_request($validaciones, $datos)) {
-			$datos['mensaje'] = 'Datos erróneos para identificar el artículo a borrar';
-			$datos['url_continuar'] = \core\URL::http('?menu=articulos');
-			$this->cargar_controlador('mensajes', '', $datos);
-			return;
-		}
-		else {
-			$clausulas['where'] = " id = {$datos['values']['id']} ";
-			if ( ! $filas = \datos\Datos_SQL::select('articulos', $clausulas)) {
-				$datos['mensaje'] = 'Error al recuperar la fila de la base de datos';
-				$this->cargar_controlador('mensajes', '', $datos);
-				return;
-			}
-			else {
-				$datos['values'] = $filas[0];
-				$datos['values']['precio'] = \core\Conversiones::decimal_punto_a_coma_y_miles($datos['values']['precio']);
-				$datos['values']['unidades_stock'] = \core\Conversiones::decimal_punto_a_coma_y_miles($datos['values']['unidades_stock']);
-			}
-		}
+		// No se utiliza este método pues se ha programado en javascript en la vista index.php de este controlador.
 		
 		$datos['contenido_principal'] = \core\Vista::generar(__FUNCTION__, $datos);
 		\core\Respuesta::enviar($datos);
 	}
 
+	
+	
+	
+	
+	
 	public function validar_form_borrar(array $datos=array())
 	{	
 		$validaciones=array(
@@ -216,36 +144,7 @@ class galeria_fotos extends \core\Controlador {
 	}
 	
 	
-	public function listado_pdf(array $datos=array()) {
-		
-		$validaciones = array(
-			"nombre" => "errores_texto"
-		);
-		\core\Validaciones::errores_validacion_request($validaciones, $datos);
-		if (isset($datos['values']['nombre'])) 
-			$select['where'] = " nombre like '%{$datos['values']['nombre']}%'";
-		$select['order_by'] = 'nombre';
-		$datos['filas'] = \datos\Datos_SQL::select('articulos', $select);		
-		
-		$datos['html_para_pdf'] = \core\Vista::generar(__FUNCTION__, $datos);
-		
-		require_once(PATH_APP."lib/php/dompdf/dompdf_config.inc.php");
-
-		$html =
-		  '<html><body>'.
-		  '<p>Put your html here, or generate it with your favourite '.
-		  'templating system.</p>'.
-		  '</body></html>';
-
-		$dompdf = new \DOMPDF();
-		$dompdf->load_html($datos['html_para_pdf']);
-		$dompdf->render();
-		$dompdf->stream("sample.pdf", array("Attachment" => 0));
-		
-		// \core\Respuesta::cambiar_tipo_mime('application/pdf');
-		// \core\Respuesta::enviar($datos, 'plantilla_pdf');
-		
-	}
+	
 	
 
 } // Fin de la clase
